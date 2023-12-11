73ff2b2a47d45b43dc642ba06e16545d09c4b804
fahrouk <48411892+fahrouk@users.noreply.github.com>
2023-12-11 13:49:13 +0000

    Create validation.js

---
+++ Static/JS/validation.js
@@ -0,0 +1,19 @@
+const SUBMIT = $("#submit");
+
+const USERNAME = $("#uname");
+const USERNAME_MSG = $("#user-msg");
+
+const PASSWORD = $("#pwd");
+const PASSWORD_MSG = $("#password-msg");
+
+const CONFIRM = $("#confirmpwd");
+const CONFIRM_MSG = $("#confirm-msg");
+
+const FNAME = $("#fname");
+const FNAME_MSG = $("#fname-msg");
+
+const LNAME = $("#lname");
+const LNAME_MSG = $("#lname-msg");
+
+const EMAIL = $("#email");
+const EMAIL_MSG = $("#email-msg");
