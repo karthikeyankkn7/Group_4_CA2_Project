7bf9e9ae03271df5b7c77efb29b276ee3d09e24b
karthikeyankkn7 <152183950+karthikeyankkn7@users.noreply.github.com>
2023-12-11 14:25:06 +0000

    Update script.js
    karthik commit script.js 1

--- Static/JS/script.js
+++ Static/JS/script.js
@@ -1,1 +1,9 @@
+$(".target").on("click", function () {
+        let $button = $(this);
+        let oldVal = parseInt($button.parent().find("input").val());
+        let newVal = 0;
+
+        if ($button.text() == '+') {
+            newVal = oldVal + 1;
+        }
 
